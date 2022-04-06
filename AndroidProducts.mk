03c154515d15c2868db58b785ef2f17679bd1be0
Akbarfr <akbareevnxx@gmail.com>
2022-04-06 13:00:01 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_ulysse.mk
+    $(LOCAL_DIR)/lineage_ulysse.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_ulysse-user \
-    aosp_ulysse-userdebug \
-    aosp_ulysse-eng
+    lineage_ulysse-user \
+    lineage_ulysse-userdebug \
+    lineage_ulysse-eng
