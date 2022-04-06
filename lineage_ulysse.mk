24993acfc6eff6dc32f9c58fbcea60249bf86d09
Akbarfr <akbareevnxx@gmail.com>
2022-04-06 12:59:22 +0700

    Update and rename aosp_ulysse.mk to lineage_ulysse.mk

--- lineage_ulysse.mk
+++ lineage_ulysse.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/ulysse/device.mk)
 
-# Inherit some common LighthouseOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ulysse
-PRODUCT_NAME := aosp_ulysse
+PRODUCT_NAME := lineage_ulysse
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 5A
